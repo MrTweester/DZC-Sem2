5239aa8bdddb6687aa86bf235eb99ae8d7dc4ed2
Islam Dagaev <spy_002@bk.ru>
2023-04-26 20:15:56 +0500

    Закоммичены домащние задания

---
+++ zad1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit2 = number/10;
+int result = digit2%10;
+Console.Write(result);
---
+++ zad2/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int digit3 = 0;
+if (number>99 && number<1000)
+{
+    digit3=number%10;
+    Console.Write($"{number} -> {digit3}");
+}
+else if(number<99)
+{
+    Console.Write($"{number} ->третьей цифры нет");
+}
+else
+{
+    while (number>1000)
+    {
+    result = number/10;
+    }
+    digit3=result%10;
+    Console.Write($"{number} -> {digit3}");
+}
+// while (number>1000)
+// {
+//     result = number/10;
+// }
+// if (number<=99)
+// {
+// Console.Write($"{number} ->третьей цифры нет");
+// }
+// else
+// {
+//     digit3 = result%10;
+//     Console.Write($"{number} -> {digit3}");
+// }
